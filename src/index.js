5c36fad062ec620f3b2a84cc0ac782be9ba8f9af
yujin mac <kimyu28jin@gmail.com>
2020-11-22 17:37:45 +0900

    redux 모듈화

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './exercise';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { rootReducer } from './modules';
+
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
